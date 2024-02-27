970a80dadc061ffa93bd3b8ccbc7f7f3106665d7
TomiHn <tomi.hanninen97@gmail.com>
2024-02-27 15:18:04 +0200

    Init

---
+++ teht_nippu_1/src/main/java/com/example/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Main {
+
+    public static int LocFind(int arr[]) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int p1[] = {};
+
+    }
+}
