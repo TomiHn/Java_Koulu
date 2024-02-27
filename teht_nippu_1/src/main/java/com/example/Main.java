518f9506ab68e3907a44e7762db3ed61daf26be1
TomiHn <tomi.hanninen97@gmail.com>
2024-02-27 17:16:18 +0200

    T1 Valmis

--- teht_nippu_1/src/main/java/com/example/Main.java
+++ teht_nippu_1/src/main/java/com/example/Main.java
@@ -2,13 +2,14 @@ package com.example;
 
 public class Main {
 
-    public static int LocFind(int arr[]) {
+    public static double distCalcl(double p1, double p2, double p3, double p4) {
 
-        return 0;
+        return Math.sqrt((Math.pow(p1 - p2, 2) + Math.pow(p3 - p4, 2)));
     }
 
     public static void main(String[] args) {
-        int p1[] = {};
+
+        System.out.println(distCalcl(1, 2, 3, 4));
 
     }
 }
