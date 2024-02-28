1c058a32a9f984a1fba4d6532c971e04c94c5559
TomiHn <tomi.hanninen97@gmail.com>
2024-02-28 11:47:29 +0200

    T5 korjattu

--- teht_nippu_1/src/main/java/com/example/Main.java
+++ teht_nippu_1/src/main/java/com/example/Main.java
@@ -39,22 +39,23 @@ public class Main {
     }
 
     public static int[] sortAlg(int[] arr) {
+        int[] arrReturn = arr.clone();
         int i, j, temp;
         boolean swapped;
-        for (i = 0; i < arr.length - 1; i++) {
+        for (i = 0; i < arrReturn.length - 1; i++) {
             swapped = false;
-            for (j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+            for (j = 0; j < arrReturn.length - i - 1; j++) {
+                if (arrReturn[j] > arrReturn[j + 1]) {
+                    temp = arrReturn[j];
+                    arrReturn[j] = arrReturn[j + 1];
+                    arrReturn[j + 1] = temp;
                     swapped = true;
                 }
             }
             if (swapped == false)
                 break;
         }
-        return arr;
+        return arrReturn;
     }
 
     public static void main(String[] args) {
